4099eb38bed730a9390332ccf16ff0ebabc7cb0b
Carolina Floro <carolinarochaf@hotmail.com>
2024-07-03 10:01:51 -0300

    .

--- js/main.js
+++ js/main.js
@@ -36,3 +36,47 @@ $(document).ready(function () {
     ],
   });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const clickableCards = document.querySelectorAll('.card-clickable');
+
+  let startX;
+  let startY;
+  let isSwiping = false;
+
+  clickableCards.forEach((card) => {
+    card.addEventListener('mousedown', function (event) {
+      console.log('mousedown');
+      startX = event.clientX;
+      startY = event.clientY;
+      isSwiping = false;
+
+      // Adiciona eventos ao document
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('mouseup', onMouseUp);
+      console.log('mouseup');
+    });
+
+    function onMouseMove(event) {
+      const dx = Math.abs(event.clientX - startX);
+      const dy = Math.abs(event.clientY - startY);
+      console.log(`DX: ${dx}`);
+      console.log(`Dy: ${dy}`);
+
+      if (dx > 5 || dy > 5) {
+        isSwiping = true;
+      }
+    }
+
+    function onMouseUp(event) {
+      document.removeEventListener('mousemove', onMouseMove);
+
+      if (!isSwiping) {
+        event.preventDefault();
+        const target = card.getAttribute('data-bs-target');
+        const modal = new bootstrap.Modal(document.querySelector(target));
+        modal.show();
+      }
+    }
+  });
+});
