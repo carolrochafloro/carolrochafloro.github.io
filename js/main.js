a5ad583d348290c8505658099bd70a3371920de0
Carolina Floro <carolinarochaf@hotmail.com>
2024-07-03 10:53:16 -0300

    .

--- js/main.js
+++ js/main.js
@@ -37,46 +37,46 @@ $(document).ready(function () {
   });
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-  const clickableCards = document.querySelectorAll('.card-clickable');
+// document.addEventListener('DOMContentLoaded', function () {
+//   const clickableCards = document.querySelectorAll('.card-clickable');
 
-  let startX;
-  let startY;
-  let isSwiping = false;
+//   let startX;
+//   let startY;
+//   let isSwiping = false;
 
-  clickableCards.forEach((card) => {
-    card.addEventListener('mousedown', function (event) {
-      console.log('mousedown');
-      startX = event.clientX;
-      startY = event.clientY;
-      isSwiping = false;
+//   clickableCards.forEach((card) => {
+//     card.addEventListener('mousedown', function (event) {
+//       console.log('mousedown');
+//       startX = event.clientX;
+//       startY = event.clientY;
+//       isSwiping = false;
 
-      // Adiciona eventos ao document
-      document.addEventListener('mousemove', onMouseMove);
-      document.addEventListener('mouseup', onMouseUp);
-      console.log('mouseup');
-    });
+//       // Adiciona eventos ao document
+//       document.addEventListener('mousemove', onMouseMove);
+//       document.addEventListener('mouseup', onMouseUp);
+//       console.log('mouseup');
+//     });
 
-    function onMouseMove(event) {
-      const dx = Math.abs(event.clientX - startX);
-      const dy = Math.abs(event.clientY - startY);
-      console.log(`DX: ${dx}`);
-      console.log(`Dy: ${dy}`);
+//     function onMouseMove(event) {
+//       const dx = Math.abs(event.clientX - startX);
+//       const dy = Math.abs(event.clientY - startY);
+//       console.log(`DX: ${dx}`);
+//       console.log(`Dy: ${dy}`);
 
-      if (dx > 5 || dy > 5) {
-        isSwiping = true;
-      }
-    }
+//       if (dx > 5 || dy > 5) {
+//         isSwiping = true;
+//       }
+//     }
 
-    function onMouseUp(event) {
-      document.removeEventListener('mousemove', onMouseMove);
+//     function onMouseUp(event) {
+//       document.removeEventListener('mousemove', onMouseMove);
 
-      if (!isSwiping) {
-        event.preventDefault();
-        const target = card.getAttribute('data-bs-target');
-        const modal = new bootstrap.Modal(document.querySelector(target));
-        modal.show();
-      }
-    }
-  });
-});
+//       if (!isSwiping) {
+//         event.preventDefault();
+//         const target = card.getAttribute('data-bs-target');
+//         const modal = new bootstrap.Modal(document.querySelector(target));
+//         modal.show();
+//       }
+//     }
+//   });
+// });
